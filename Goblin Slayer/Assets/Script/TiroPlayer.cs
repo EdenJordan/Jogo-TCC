76173e5e4f4af4ab70c5ee21298aa2ff313dbea2
Jackson <antoniojacksontenorio@gmail.com>
2023-07-05 11:36:17 -0300

    Upload 2

---
+++ Goblin Slayer/Assets/Script/TiroPlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiroPlayer : MonoBehaviour
+{
+    public Rigidbody2D rig;
+    public GameObject tiro1;
+    public float speed;
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
